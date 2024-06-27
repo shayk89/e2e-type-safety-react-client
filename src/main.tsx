14970b655fa932ffb8d2bd97a71d6cf13fc74fbd
Shay K <shaykemp89@gmail.com>
2024-06-27 11:23:20 -0700

    commiting basic functioning frontend

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { cacheExchange, createClient, fetchExchange, Provider } from 'urql'
+
+const client = createClient({
+  url: import.meta.env.VITE_API_URL || 'http://localhost:4000/graphql',
+  exchanges: [cacheExchange, fetchExchange]
+});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider value={client}>
+      <App />
+    </Provider>    
+  </React.StrictMode>
+)
